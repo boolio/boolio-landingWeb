65eb837a1be544a73aa27e48d70ffcaebc550ea5
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2015-05-06 20:58:49 -0400

    FIX: new landing page sex

---
+++ js/fade.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+      $(".container").animate({right:200, opacity:"show"},2000);
+      $(".pic-orient").animate({right:200, opacity:"show"},3000);
+      $(".boolio-subtext").fadeIn(2000);
+});
